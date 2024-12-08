fb4ec58384d6bf3df33abf01fe1f1ad3432ec7ac
Aayush Singh <aayushsingh1239@gmail.com>
2024-12-08 17:29:50 +0545

    Music player with javascript

--- Music-player/index.js
+++ Music-player/index.js
@@ -172,9 +172,9 @@ const renderSongs = (array) => {
             return `
       <li id="song-${song.id}" class="playlist-song">
       <button class="playlist-song-info" onclick="playSong(${song.id})">
-          <span class="playlist-song-title">${song.title}</span>
-          <span class="playlist-song-artist">${song.artist}</span>
-          <span class="playlist-song-duration">${song.duration}</span>
+            <span class="playlist-song-title">${song.title}</span>
+            <span class="playlist-song-artist">${song.artist}</span>
+            <span class="playlist-song-duration">${song.duration}</span>
       </button>
       <button onclick="deleteSong(${song.id})" class="playlist-song-delete" aria-label="Delete ${song.title}">
           <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/>
